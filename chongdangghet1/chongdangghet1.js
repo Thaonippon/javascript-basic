7885aa62fdcedd84a74da3c8f65aa6dc68c383a2
thaonnippon <phamthao19921011@gmail.com>
2025-07-11 22:26:51 +0700

    source 20250710

---
+++ chongdangghet1/chongdangghet1.js
@@ -0,0 +1,10 @@
+
+// const x = 5;
+
+// x = 6;
+console.log('hello chong dang iu')
+
+let x = 5, y = 6, z = 7;
+
+console.log("hello chong dang iu" + ', x=' + x + ', y=' + y + ', z=' + z);
+console.log("hello chong dang iu 123", 'x=', x, 'y=', y, 'z=', z);
